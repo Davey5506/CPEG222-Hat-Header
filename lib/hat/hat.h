0b7c5c9e595fa920cd7b5bd41a2da856115c2aa1
David <kpdmmvi@gmail.com>
2025-10-16 01:40:16 -0400

    Improved functionallity of exsisting functions. Added a function to initialize timer interrupts.

--- lib/hat/hat.h
+++ lib/hat/hat.h
@@ -64,6 +64,11 @@ void set_pin_pull(GPIO_TypeDef* GPIOx, uint8_t pin, PIN_PULL pull);
 void set_output_type(GPIO_TypeDef* GPIOx, uint8_t pin, PIN_OUTPUT_TYPE type);
 void write_pin(GPIO_TypeDef* GPIOx, uint8_t pin, PIN_VALUE value);
 uint8_t read_pin(GPIO_TypeDef* GPIOx, uint8_t pin);
+void toggle_pin(GPIO_TypeDef* GPIOx, uint8_t pin);
+void init_sys_tick(uint32_t ticks);
+void delay_us(uint32_t us);
+void init_gp_timer(TIM_TypeDef* TIMx, uint32_t freq, uint32_t arr);
+void init_timer_IRQ(TIM_TypeDef* TIMx, uint16_t priority);
 void init_ssd (uint16_t reload_time);
 void display_num(uint16_t num, uint8_t decimal_place);
 
